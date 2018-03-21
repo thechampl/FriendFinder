6b0cabf3b15bc21abdbb611255d4949467c328f3
Luke Champ <luke.champ@outlook.com>
2018-03-21 15:06:56 -0400

    logic for finding differnce in scores done, working on getting indexOf min number then displaying data

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./routing/routes.js")(app);
